247d1d732041b6fb5f2bc271b7d09a3ed91d9eac
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-03 15:25:53 -0300

    Change default home page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    coolGram
                 </div>
 
                 <div class="links">
