c82dd32d1f2bbf2877c2e3276d22665a9c14f5c8
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-04 23:40:13 -0300

    Create static profile home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,37 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    {{-- TODO: align to center profile picture and information. --}}
+    <div class="row">
+        <div class="col-3 p-5">
+            {{-- 180 x 180 resolution --}}
+            <img src="/img/profile-test.png" class="rounded-circle" alt=""> 
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>coolgram</h1></div>
+            <div class="d-flex pt-1 ">
+                <div class="pr-5"><strong>110</strong> posts</div>
+                <div class="pr-5"><strong>491k</strong> followers</div>
+                <div class="pr-5"><strong>42</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">coolgram.org</div>
+            <div>Like ig. But cooler.</div>
+            <div><a href="#">Visit our site</a></div>
+        </div>
+    </div>
+    {{-- TODO: check if post image size can be smaller. --}}
+    <div class="row pt-4">
+        <div class="col-4">
+            {{-- 640 x 640 resolution --}}
+            <img src="/img/img-1.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/img-2.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/img/img-3.jpg" class="w-100">
         </div>
     </div>
 </div>
 @endsection
+    
