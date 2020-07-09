6a0bf8c88aec27ec0b7d9f6bb7783c990724964b
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-09 14:19:53 -0300

    Get the file path of the image to store in db and store the actual image in the storage dir

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -25,8 +25,13 @@ class PostsController extends Controller
             'image' => 'required|image',
         ]);
 
-        auth()->user()->posts()->create($data);
+        $img_path = $data['image']->store('uploads', 'public');
 
-        dd(request()->all());
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $img_path
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
