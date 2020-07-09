01b36e1768298e372c1b8f8769453ae48159e885
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-09 12:10:49 -0300

    Add link to new post creation in profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,7 @@
         <div class="col-8 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a class="btn btn-primary" href="#">New Post</a>
+                <a class="btn btn-primary" href="/p/create">New Post</a>
             </div>
             <div class="d-flex pt-1 ">
                 <div class="pr-5"><strong>110</strong> posts</div>
