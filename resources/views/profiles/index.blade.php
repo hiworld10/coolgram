7160ac1e859258efe4514429891568968a45927e
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-10 14:25:53 -0300

    Update profile index view

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-3 p-5 ml-auto">
             {{-- 180 x 180 resolution --}}
-            <img src="{{ isset($user->profile->image) ? '/storage/' . $user->profile->image : '/img/nophoto.png' }}" class="rounded-circle" alt="profile.jpeg"> 
+            <img src="{{ isset($user->profile->image) ? '/storage/' . $user->profile->image : '/img/nophoto.png' }}" class="rounded-circle w-100" alt="profile.jpeg"> 
         </div>
         <div class="col-8 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
@@ -19,8 +19,8 @@
                 @endcan
             <div class="d-flex pt-1 ">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-5"><strong>491k</strong> followers</div>
-                <div class="pr-5"><strong>42</strong> following</div>
+                <div class="pr-5"><strong>0</strong> followers</div>
+                <div class="pr-5"><strong>0</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
@@ -31,7 +31,9 @@
     @if (count($user->posts) <= 0)
         <div class="pt-4 text-center">
             <h3>No posts yet.</h3>
-            <p class="pt-3">Add your first one <a href="/p/create">here.</a></p>    
+            @can('update', $user->profile)
+                <p class="pt-3">Add your first one <a href="/p/create">here.</a></p>    
+            @endcan
         </div>
     @else
         <div class="row pt-4">
