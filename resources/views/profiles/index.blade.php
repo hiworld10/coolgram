c763afbb6363b2c0383b501f27649440544a57b2
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-11 18:17:19 -0300

    Add followers and following count in profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -23,8 +23,8 @@
                 @endcan
             <div class="d-flex pt-1 ">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-5"><strong>0</strong> followers</div>
-                <div class="pr-5"><strong>0</strong> following</div>
+                <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
