25eeb3affca0a2e8a5c20a27f7b2933137211851
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-13 19:42:15 -0300

    Adjust div columns for showing posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-8">
+        <div class="col-7">
             <img src="/storage/{{ $post->image }}" class="w-100" alt="image.jpeg">
         </div>
-        <div class="col-4">
+        <div class="col-5">
             <div>
                 <div class="d-flex align-items-center">
                     <div class="pr-3">
