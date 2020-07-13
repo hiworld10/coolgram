7adfa3f0ee1ea20b63a44390f95148b06b683798
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-12 21:01:20 -0300

    Add a not following any accounts warning

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,48 +2,54 @@
 
 @section('content')
 <div class="container">
-    @foreach ($posts as $post)
-
-        <div class="row">
-            <div class="col-6 offset-3 d-flex align-items-center pb-3">
-                <div class="pr-3">
-                    <img src="{{ $post->user->profile->imageSrc() }}" class="w-100 rounded-circle" style="max-width: 40px;" alt="profile.jpeg">    
-                </div>
-
-                <div>
-                    <div class="font-weight-bold">
-                        <a class="pr-1" href="/profile/{{$post->user->id}}">
-                            <span class="text-dark">{{ $post->user->username }}</span>
-                        </a>
+    @if (count($posts) <= 0)
+        <div class="pt-4 text-center">
+            <div>You are currently not following any accounts.</div>
+            <div class="pt-2">Start following accounts to see posts displayed here.</div>
+        </div>    
+    @else
+        @foreach ($posts as $post)
+            <div class="row">
+                <div class="col-6 offset-3 d-flex align-items-center pb-3">
+                    <div class="pr-3">
+                        <img src="{{ $post->user->profile->imageSrc() }}" class="w-100 rounded-circle" style="max-width: 40px;" alt="profile.jpeg">    
                     </div>
-                </div>                
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-6 offset-3">
-                <a href="/p/{{ $post->id }}">
-                    <img src="/storage/{{ $post->image }}" class="w-100" alt="image.jpeg">
-                </a>
-            </div>
-        </div>
-
-        <div class="row pt-3 pb-2">
-            <div class="col-6 offset-3">
-                <div>
-
-                    <p>
-                        <span class="font-weight-bold">
-                            <a href="/profile/{{$post->user->id}}">
+                    <div>
+                        <div class="font-weight-bold">
+                            <a class="pr-1" href="/profile/{{$post->user->id}}">
                                 <span class="text-dark">{{ $post->user->username }}</span>
                             </a>
-                        </span>
-                        {{ $post->caption }}
-                    </p>
-                    <hr>
+                        </div>
+                    </div>                
                 </div>
             </div>
-        </div>
-    @endforeach
+
+            <div class="row">
+                <div class="col-6 offset-3">
+                    <a href="/p/{{ $post->id }}">
+                        <img src="/storage/{{ $post->image }}" class="w-100" alt="image.jpeg">
+                    </a>
+                </div>
+            </div>
+
+            <div class="row pt-3 pb-2">
+                <div class="col-6 offset-3">
+                    <div>
+
+                        <p>
+                            <span class="font-weight-bold">
+                                <a href="/profile/{{$post->user->id}}">
+                                    <span class="text-dark">{{ $post->user->username }}</span>
+                                </a>
+                            </span>
+                            {{ $post->caption }}
+                        </p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @endif
 </div>
 @endsection
